4ee06b3c4c13ddf158dbe805d1f091fa2f4f1d18
asacxyz <andrealck1@gmail.com>
2025-04-20 18:22:12 -0300

    chore: init

---
+++ src/main/java/com/asacxyz/peekr/PeekrApplication.java
@@ -0,0 +1,13 @@
+package com.asacxyz.peekr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeekrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeekrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asacxyz/peekr/PeekrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asacxyz.peekr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeekrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
